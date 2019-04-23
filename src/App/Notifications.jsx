32b3f04a8a1c096ef87aee3512f551989199170a
liucong211463 <liucong211463@sogou-inc.com>
2019-04-23 22:45:17 +0800

    fix: level up snackbar

--- src/App/Notifications.jsx
+++ src/App/Notifications.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
-import { Slide, SnackbarContent, IconButton } from '@material-ui/core';
+import {
+  Slide, SnackbarContent, IconButton, Portal,
+} from '@material-ui/core';
 import { Close as CloseIcon } from '@material-ui/icons';
 import Box from '@material-ui/core/Box';
 import { green, amber } from '@material-ui/core/colors';
@@ -25,43 +27,55 @@ const useStyles = makeStyles(theme => ({
   icon: {
     fontSize: 18,
   },
+  root: {
+    zIndex: theme.zIndex.snackbar,
+  },
 }));
 
 function Notifications(props) {
   const classes = useStyles();
 
   return (
-    <Box position="fixed" bottom={0} left={0} mx={2} my={2}>
-      <TransitionGroup>
-        {props.notifications.reverse().map(notification => (
-          <Slide direction="right" key={notification.id}>
-            <Box mt={1}>
-              <SnackbarContent
-                classes={{
-                  root: classes[notification.type],
-                }}
-                message={notification.message.split(/\r\n/g).map(msg => <div key={msg}>{msg}</div>)}
-                action={[
-                  <IconButton
-                    color="inherit"
-                    onClick={() => {
-                      props.dispatch(removeNotification(notification.id));
-                    }}
-                    key="close"
-                  >
-                    <CloseIcon
-                      classes={{
-                        root: classes.icon,
+    <Portal>
+      <Box
+        position="fixed"
+        bottom={0}
+        left={0}
+        mx={2}
+        my={2}
+        className={classes.root}
+      >
+        <TransitionGroup>
+          {props.notifications.reverse().map(notification => (
+            <Slide direction="right" key={notification.id}>
+              <Box mt={1}>
+                <SnackbarContent
+                  classes={{
+                    root: classes[notification.type],
+                  }}
+                  message={notification.message.split(/\r\n/g).map(msg => <div key={msg}>{msg}</div>)}
+                  action={[
+                    <IconButton
+                      color="inherit"
+                      onClick={() => {
+                        props.dispatch(removeNotification(notification.id));
                       }}
-                    />
-                  </IconButton>,
-                ]}
-              />
-            </Box>
-          </Slide>
-        ))}
-      </TransitionGroup>
-    </Box>
+                      key="close"
+                    >
+                      <CloseIcon
+                        classes={{
+                          root: classes.icon,
+                        }}
+                      />
+                    </IconButton>,
+                  ]}
+                />
+              </Box>
+            </Slide>
+          ))}
+        </TransitionGroup>
+      </Box>
+    </Portal>
   );
 }
 
